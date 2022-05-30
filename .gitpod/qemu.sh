9537eba137a33150896880216c0818ae858d088b
Denis GERMAIN <dt.germain@gmail.com>
2022-05-30 13:31:03 +0000

    fix: readd linux-image and upgrade apt source to jammy

--- .gitpod/qemu.sh
+++ .gitpod/qemu.sh
@@ -6,7 +6,7 @@ script_dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 outdir="${script_dirname}/_output"
 
 sudo qemu-system-x86_64 -kernel "/boot/vmlinuz" \
--boot c -m 2049M -hda "${outdir}/rootfs/hirsute-server-cloudimg-amd64.img" \
+-boot c -m 2049M -hda "${outdir}/rootfs/jammy-server-cloudimg-amd64.img" \
 -net user \
 -smp 8 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
