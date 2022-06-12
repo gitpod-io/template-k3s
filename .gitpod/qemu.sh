ffab450be7b0b0ffe6e489e8008339ad86697825
Denis GERMAIN <dt.germain@gmail.com>
2022-06-12 20:48:46 +0000

    chore: move wait apt aside

--- .gitpod/qemu.sh
+++ .gitpod/qemu.sh
@@ -2,24 +2,8 @@
 
 set -xeuo pipefail
 
-function waitapt() {
-  i=0
-  tput sc
-  while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-      case $(($i % 4)) in
-          0 ) j="-" ;;
-          1 ) j="\\" ;;
-          2 ) j="|" ;;
-          3 ) j="/" ;;
-      esac
-      tput rc
-      echo -en "\r[$j] Waiting for other software managers to finish..." 
-      sleep 0.5
-      ((i=i+1))
-  done
-}
+./wait-apt.sh
 
-waitapt
 sudo apt update -y
 sudo apt install qemu qemu-system-x86 linux-image-generic -y
 
